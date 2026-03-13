05e5ac7f6f21cba1002206f5da6f91ef601c42ec
midhila345 <midhilanair1.1@gmail.com>
2026-03-13 14:36:04 +0530

    updated file

--- script.js
+++ script.js
@@ -76,3 +76,38 @@ document.querySelectorAll('nav a[href^="#"]').forEach(anchor=>{
   });
 });
 
+
+const projectItems = document.querySelectorAll(".project-item");
+const modal = document.getElementById("gallery-modal");
+const galleryContent = document.querySelector(".gallery-content");
+const closeGallery = document.getElementById("close-gallery");
+
+
+projectItems.forEach(item => {
+  item.addEventListener("click", () => {
+
+    galleryContent.innerHTML = "";
+
+    const images = item.querySelectorAll("img");
+
+    images.forEach(img => {
+      const newImg = document.createElement("img");
+      newImg.src = img.src;
+      galleryContent.appendChild(newImg);
+    });
+
+    modal.style.display = "flex";
+  });
+});
+
+
+closeGallery.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
